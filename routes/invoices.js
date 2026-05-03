c97a83bf01250406385e36f50d901e9bbe693835
Sanuda personal <sanudadilwan123@gmail.com>
2026-05-03 23:47:24 +0530

    Update invoices.js

--- routes/invoices.js
+++ routes/invoices.js
@@ -3,3 +3,13 @@ const router = express.Router();
 const Invoice = require('../models/Invoice');
 const PDFDocument = require('pdfkit'); // IMPORT PDFKIT HERE
 
+// CREATE
+router.post('/', async (req, res) => {
+  try {
+    const newInvoice = new Invoice(req.body);
+    await newInvoice.save();
+    res.status(201).json(newInvoice);
+  } catch (error) { 
+    res.status(500).json({ message: "Error adding invoice" }); 
+  }
+});
