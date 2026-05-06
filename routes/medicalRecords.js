2b9bddfed39712d9308cd2003534f60dd2814cec
Sanuda personal <sanudadilwan123@gmail.com>
2026-05-07 00:11:41 +0530

    Update medicalRecords.js

--- routes/medicalRecords.js
+++ routes/medicalRecords.js
@@ -37,3 +37,12 @@ router.post('/', async (req, res) => {
   }
 });
 
+// GET: ADMIN FETCH ALL
+router.get('/', async (req, res) => {
+  try {
+    const allRecords = await MedicalRecord.find();
+    res.status(200).json(allRecords);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
