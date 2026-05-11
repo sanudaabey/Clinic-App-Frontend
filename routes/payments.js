e0fe73e86b0e9b0039ef92e173e8cb607d638617
Sanuda personal <sanudadilwan123@gmail.com>
2026-05-12 01:06:34 +0530

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -4,3 +4,7 @@ const Payment = require('../models/Payment');
 const Invoice = require('../models/Invoice'); 
 // NEW: Import the Notification model so we can create alerts!
 const Notification = require('../models/Notification'); 
+
+// STRIPE DEPENDENCY
+const stripe = require('stripe')('sk_test_51TL770Axgv4sFwCFUgBYpr7RpaYbpiBqcnqmk1rjfNfkEpCdflC4bBlL7E3a0dtpdTjaKcyJgtDX1Wh5w1Uf6l9w00FwrljmBw'); 
+
