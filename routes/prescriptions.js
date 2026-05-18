e57fa9a981016a8bd9432c7a3a402daeca41991f
Sanuda personal <sanudadilwan123@gmail.com>
2026-05-18 14:57:13 +0530

    Update prescriptions.js

--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -2,3 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Prescription = require('../models/Prescription');
 
+// CREATE
+router.post('/', async (req, res) => {
+  try {
+    const newPrescription = new Prescription(req.body);
+    await newPrescription.save();
+    res.status(201).json(newPrescription);
+  } catch (error) { 
+    console.error("PRESCRIPTION SAVE ERROR:", error); 
+    res.status(500).json({ message: "Error adding prescription" }); 
+  }
+});
